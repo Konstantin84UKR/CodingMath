8d67fb162bb3142e844ad63729ef59eb364625a3
Konstantin84UKR <110284bkj@gmail.com>
2024-06-30 19:06:46 +0300

    update

--- unit13/Particle.js
+++ unit13/Particle.js
@@ -24,26 +24,26 @@ export class Particle {
 
 	}
 
-	angleTo(p2) {
-		return Math.atan2(p2.position.getY() - this.position.getY(),
-			p2.position.getX() - this.position.getX());
-	}
+	// angleTo(p2) {
+	// 	return Math.atan2(p2.position.getY() - this.position.getY(),
+	// 		p2.position.getX() - this.position.getX());
+	// }
 
-	distanceTo(p2) {
-		const dx = p2.position.getX() - this.position.getX();
-		const dy = p2.position.getY() - this.position.getY();
+	// distanceTo(p2) {
+	// 	const dx = p2.position.getX() - this.position.getX();
+	// 	const dy = p2.position.getY() - this.position.getY();
 
-		return Math.sqrt(dx * dx + dy * dy);
-	}
+	// 	return Math.sqrt(dx * dx + dy * dy);
+	// }
 
-	gravityTo(p2) {
-		const dist = this.distanceTo(p2);
-		let grav = new Vector(0, 0);
-		grav.setLength(p2.mass / (dist * dist));
-		grav.setAngle(this.angleTo(p2));
+	// gravityTo(p2) {
+	// 	const dist = this.distanceTo(p2);
+	// 	let grav = new Vector(0, 0);
+	// 	grav.setLength(p2.mass / (dist * dist));
+	// 	grav.setAngle(this.angleTo(p2));
 
-		this.velocity.addTo(grav);
+	// 	this.velocity.addTo(grav);
 
-	}
+	// }
 
 }
